3af794f97cab4c0b5d909e237d223dc235f97ede
Max Wanless <maxwanless@gmail.com>
2022-09-07 10:33:01 -0400

    Fix warehouse route

--- index.js
+++ index.js
@@ -12,9 +12,11 @@ app.use(cors());
 //Middleware to accces the body of the request
 app.use(express.json());
 
+//Warehouse route
+app.use("/", warehouseRoute);
 //Inventory route
 app.use("/inventory", inventoryRoute);
-app.use("/warehouse", warehouseRoute);
+
 
 app.listen(8080, () => {
   console.log("Running on: " + PORT);
