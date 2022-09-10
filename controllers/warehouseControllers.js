a4706d832f4e76e58cc7a7738673a94da51950f4
Graziela Pauselius <grazielak.pauselius@gmail.com>
2022-09-09 18:09:05 -0700

    Create validation function

--- controllers/warehouseControllers.js
+++ controllers/warehouseControllers.js
@@ -7,41 +7,99 @@ let warehouseData = warehouseModel.fetchWarehouseData();
 
 //------- Get all Warehouses List ----------
 const warehouseList = (_req, res) => {
-  res.status(200).json({
-    status: "success",
-    results: warehouseData.length,
-    warehouseData,
-  });
+	res.status(200).json({
+		status: "success",
+		results: warehouseData.length,
+		warehouseData,
+	});
 };
 
 // ---------- Get all inventory items for a Warehouse --------
 const warehouseInventory = (req, res) => {
-  const id = req.params.id;
-  const warehouseInventory = inventoryData.filter(
-    (list) => list.warehouseID === id
-  );
-  res.status(200).json({
-    status: "success",
-    warehouseInventory,
-  });
+	const id = req.params.id;
+	const warehouseInventory = inventoryData.filter(
+		(list) => list.warehouseID === id
+	);
+	res.status(200).json({
+		status: "success",
+		warehouseInventory,
+	});
 };
 
 // ---------- Delete Warehouse and ascociated inventory --------
 const deleteWarehouse = (req, res) => {
-  let warehouses = warehouseModel.fetchWarehouseData();
-  let currentwarehouse = warehouses.find(
-    (warehouse) => warehouse.id === req.params.id
-  );
-  warehouses.splice(warehouses.indexOf(currentwarehouse), 1);
-  warehouseModel.writeWarehouseData(warehouses);
-  let inventory = inventoryModel.fetchInventoryData();
-  inventory = inventory.filter((item) => item.warehouseID !== req.params.id);
-  inventoryModel.writeInventoryData(inventory);
-  res.status(200).json(currentwarehouse);
+	let warehouses = warehouseModel.fetchWarehouseData();
+	let currentwarehouse = warehouses.find(
+		(warehouse) => warehouse.id === req.params.id
+	);
+	warehouses.splice(warehouses.indexOf(currentwarehouse), 1);
+	warehouseModel.writeWarehouseData(warehouses);
+	let inventory = inventoryModel.fetchInventoryData();
+	inventory = inventory.filter((item) => item.warehouseID !== req.params.id);
+	inventoryModel.writeInventoryData(inventory);
+	res.status(200).json(currentwarehouse);
+};
+
+//------ Check empty fields Function  ------
+
+const checkFields = (req, res, next) => {
+	if (
+		!req.body.name ||
+		!req.body.address ||
+		!req.body.city ||
+		!req.body.country ||
+		!req.body.contact
+	)
+		return res.status(404).json({
+			status: "fail",
+			message: "Please, fill all the fields! ",
+		});
+	next();
+};
+
+//------------ Validation Function -----------
+const fieldValidation = (req, res, next) => {
+	const { email } = req.body.contact;
+	const { phone } = req.body.contact;
+
+	const validEmail = warehouseData.find((el) => el.contact.email === email);
+	const validPhone = warehouseData.find((el) => el.contact.phone === phone);
+
+	res.status(200).json({
+		status: "sucess",
+		mensagem: "Email and Phone Number validated!",
+	});
+	next();
+};
+
+//------- Put Request Warehouse ----------
+const editWareHouse = (req, res) => {
+	const id = req.params.id;
+	const newWarehouse = warehouseData.find((el) => el.id === id);
+
+	if (newWarehouse) {
+		let editedItem = {
+			id: req.body.id,
+			name: req.body.name,
+			address: req.body.address,
+			city: req.body.city,
+			country: req.body.country,
+			contact: {
+				name: req.body.contact.name,
+				position: req.body.contact.position,
+				phone: req.body.contact.phone,
+				email: req.body.contact.email,
+			},
+		};
+		return editedItem;
+	}
+	res.status(200).json({
+		editedItem,
+	});
 };
 
 module.exports = {
-  warehouseList,
-  warehouseInventory,
-  deleteWarehouse,
+	warehouseList,
+	warehouseInventory,
+	deleteWarehouse,
 };
